e9909b69b1a3769270627c682e0520a0435d9b05
User <user@example.com>
2026-06-21 03:36:58 +0500

    Use  env var for Railway deployment

--- backend/run.py
+++ backend/run.py
@@ -6,9 +6,10 @@ if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 import uvicorn
+import os
 
 if __name__ == "__main__":
     print("Starting Needstack Backend...")
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=8002, reload=True, loop="asyncio")
+    config = uvicorn.Config("main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8002")), reload=True, loop="asyncio")
     server = uvicorn.Server(config)
     asyncio.run(server.serve())
